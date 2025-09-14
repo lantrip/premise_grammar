b2f95edfe3f5ad03add6835cbfbfa118c00b2ef8
David Lantrip <dave@lant.rip>
2025-09-14 14:51:29 -0400

    Add semantic story-focused scopes for flexible theming
    - Updated highlights.scm with story-specific semantic scopes
    - Added distinct scopes for beats, treatment, narrative content types
    - Added story structure scopes (act, scene, cel) with specific contexts
    - Added entity reference scopes for character/location highlighting
    - Added author comment vs story content distinction
    - Enables flexible theming for different story author workflows

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,30 +1,31 @@
 ; Cuneiform Syntax Highlighting
 ; Simplified version without field references
 
-; Comments
-(line_comment) @comment
+; Comments - distinguish from story content
+(line_comment) @comment.line
 
-; File headers
-(file_header) @keyword.control
+; File headers - metadata about the story
+(file_header) @keyword.directive
 
-; Structure headers
-(act_header) @markup.heading.1
-(scene_header) @markup.heading.2  
-(cel_header) @markup.heading.3
+; Structure headers - story organization
+(act_header) @markup.heading.1.story.act
+(scene_header) @markup.heading.2.story.scene
+(cel_header) @markup.heading.3.story.cel
 
-; Header markers
-"=" @punctuation.special
-"==" @punctuation.special
-"===" @punctuation.special
+; Header markers - visual hierarchy
+"=" @punctuation.definition.heading.story.act
+"==" @punctuation.definition.heading.story.scene
+"===" @punctuation.definition.heading.story.cel
 
-; Content type markers
-(content_type_beat) @keyword.control
-(content_type_treatment) @keyword.control  
-(content_type_narrative) @keyword.control
+; Content type markers - story layer indicators
+(content_type_beat) @keyword.control.content.beat
+(content_type_treatment) @keyword.control.content.treatment
+(content_type_narrative) @keyword.control.content.narrative
 
-"///" @punctuation.special
-"//" @punctuation.special
-"/" @punctuation.special
+; Content type punctuation - visual cues for story layers
+"///" @punctuation.definition.content.beat
+"//" @punctuation.definition.content.treatment
+"/" @punctuation.definition.content.narrative
 
 ; Entity definitions
 (entity_construct) @variable.special
@@ -46,15 +47,15 @@
 ; Metadata
 (metadata_line) @property
 
-; Entity references
-(entity_reference) @variable.special
-(ref_open) @punctuation.special
-(ref_close) @punctuation.special
+; Entity references - character/location/item names in story
+(entity_reference) @entity.name.reference
+(ref_open) @punctuation.definition.entity.begin
+(ref_close) @punctuation.definition.entity.end
 
-; Dialogue and prose
-(dialogue_speaker) @character
-(parenthetical) @comment.doc
-(prose_text) @text
+; Story content - different types of narrative text
+(dialogue_speaker) @entity.name.character.speaker
+(parenthetical) @text.parenthetical
+(prose_text) @text.narrative
 
 ; Punctuation
 ":" @punctuation.delimiter
